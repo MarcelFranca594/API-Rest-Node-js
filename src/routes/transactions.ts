1e4e342158ec570601c8af56d22eb85bd17a325f
marcelfranca594@gmail.com <marcelfranca594@gmail.com>
2024-03-06 08:53:16 -0400

    Tipagem no Knex - Listagem das transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,35 @@ import { randomUUID } from 'node:crypto'
 import { knex } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  // Criando uma rota de listagem
+  app.get('/', async () => {
+    // Buscando todas as transações do banco de dados usando o knex e armazenando na variável transactions
+    const transactions = await knex('transactions').select()
+
+    // Retornando um objeto contendo as transações
+    return {
+      transactions,
+    }
+  })
+
+  // Criando uma rota que mostra detalhes de uma transação com base em seu ID
+  // Exemplo de chamada: http://localhost:3333/transactions/skjdbks-154d5s-sds-12254
+  app.get('/:id', async (request) => {
+    // Definindo um esquema para validar os parâmetros da requisição, garantindo que o ID seja uma string UUID válida
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    // Validando os parâmetros da requisição com base no esquema definido e extraindo o ID da requisição
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    // Buscando a transação no banco de dados com base no ID fornecido e armazenando na variável transaction
+    const transaction = await knex('transactions').where('id', id).first()
+
+    // Retornando um objeto contendo os detalhes da transação encontrada
+    return { transaction }
+  })
+
   // Criando uma rota POST em '/' na instância do Fastify
   app.post('/', async (request, reply) => {
     // { title, amount, type: credit ou debit}
